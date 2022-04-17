4dfa488f0e0f1b420149a25310ea3b6c830fc0f6
Alex <lxkrmr@icloud.com>
2022-04-17 19:26:18 +0200

    Backend setup

---
+++ src/main/java/com/example/springbootrestreact/payroll/DatabaseLoader.java
@@ -0,0 +1,21 @@
+package com.example.springbootrestreact.payroll;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeRepository repository;
+
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.repository.save(new Employee("Frodo",
+                                          "Baggins",
+                                          "ring bearer"));
+    }
+}
